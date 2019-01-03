616f83c73ed508902c1d38aa1f54e9a9407c1057
Leon Shainski <lshai078@uottawa.ca>
2019-01-02 22:21:43 -0500

    Implementing Jquery elements for scrolling effects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+jquery(document).ready(function($) {
 
+  var bgImage = $(".background-image"),
+      frImage = $("front-pic"),
+      scrollLine=$(".scroll-line"),
+      scrollDown=$(".scrolldown"),
+      titleMain=$(".title-main")
+
+
+});
