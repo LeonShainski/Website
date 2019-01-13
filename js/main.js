671ae4a490f2cc8bb042561c165c19be0abe16fa
Leon Shainski <leon.shainski@gmail.com>
2019-01-12 21:15:50 -0500

    Adding lots of animated text

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 jQuery(document).ready(function($) {
 
   var bgImage = $(".background-image"),
@@ -61,4 +62,102 @@ var tlMainScrollOut = new TimelineMax()
   .setTween(tlMainScrollOut)
   .setPin('.section2')
   .addTo(controller);
+
+
+
+
+
 });
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700
+  }).add({
+    targets: '.ml5 .line',
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: function(e, i, l) {
+      var offset = -0.625 + 0.625*2*i;
+      return offset + "em";
+    }
+  }).add({
+    targets: '.ml5 .ampersand',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=300'
+  }).add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 10000
+  });
+
+
+
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml8 .circle-white',
+      scale: [0, 3],
+      opacity: [1, 0],
+      easing: "easeInOutExpo",
+      rotateZ: 360,
+      duration: 1100
+    }).add({
+      targets: '.ml8 .circle-container',
+      scale: [0, 1],
+      duration: 1100,
+      easing: "easeInOutExpo",
+      offset: '-=1000'
+    }).add({
+      targets: '.ml8 .circle-dark',
+      scale: [0, 1],
+      duration: 1100,
+      easing: "easeOutExpo",
+      offset: '-=600'
+    }).add({
+      targets: '.ml8 .letters-left',
+      scale: [0, 1],
+      duration: 1200,
+      offset: '-=550'
+    }).add({
+      targets: '.ml8 .bang',
+      scale: [0, 1],
+      rotateZ: [45, 15],
+      duration: 1200,
+      offset: '-=1000'
+    }).add({
+      targets: '.ml8',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 14000
+    });
+
+  anime({
+    targets: '.ml8 .circle-dark-dashed',
+    rotateZ: 360,
+    duration: 8000,
+    easing: "linear",
+    loop: true
+  });
